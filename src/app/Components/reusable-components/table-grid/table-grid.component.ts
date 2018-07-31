ffa57bb197b60da25df44af212d64820fbb11d47
rahul <rahul110794@gmail.com>
2018-07-31 19:06:59 +0530

    table fixes

--- src/app/Components/reusable-components/table-grid/table-grid.component.ts
+++ src/app/Components/reusable-components/table-grid/table-grid.component.ts
@@ -22,6 +22,7 @@ export class TableGridComponent implements OnInit, OnChanges {
   public parameters;
   public _tableHeadingNames;
   public sortOrder=[];
+  public columnWidth=[];
   public count = 0;
   constructor(private renderer: Renderer,private _storesservice:StoresServiceService,
     private _route: ActivatedRoute,
@@ -30,6 +31,9 @@ export class TableGridComponent implements OnInit, OnChanges {
   }
 
   ngOnInit() {
+    if(localStorage.getItem('columnWidth')){
+      this.columnWidth = localStorage.getItem('columnWidth').split(",");
+    }
   }
 
   public getRouteParams() {
@@ -48,18 +52,11 @@ export class TableGridComponent implements OnInit, OnChanges {
     }
   }  
 
-  private onMouseDown(event){
-  this.start = event.target;
-  this.pressed = true;
-  this.startX = event.x;
-  this.startWidth = $(this.start).parent().width();
-  this.initResizableColumns();
-  }
-
+  
   public getTableDetails(): Observable<any> | any {
-      return this.serviceCall[this.functionCall](this.getRouteParams()).do(this.processData);
+    return this.serviceCall[this.functionCall](this.getRouteParams()).do(this.processData);
   }
-
+  
   public processData = (datasource) => {
     
     if (!this._tableHeadingNames) {
@@ -68,36 +65,62 @@ export class TableGridComponent implements OnInit, OnChanges {
       for(let i=0;i<this._tableHeadingNames.length;i++){
         this.sortOrder.push(true);
       }
+      if(this.columnWidth.length==0){
+        for(let i=0;i<this._tableHeadingNames.length;i++){
+          this.columnWidth.push(($(window).width()-120)/this._tableHeadingNames.length); 
+        }
+      }
+      console.log(this.columnWidth)
+      localStorage.setItem("columnWidth",this.columnWidth.join());
     }
     this.parameters = {
-        limit: 50,
-        sortBy: this.parameters.sortBy,
+      limit: 50,
+      sortBy: this.parameters.sortBy,
         sortDir: this.parameters.sortDir,
         startFrom: Number(this.parameters['limit']) + Number(this.parameters['startFrom'])
       };
     if(datasource.length>0){
     this.tableData = this.tableData.concat(datasource);
     this._router.navigate([], { queryParams: this.parameters });  
-    }
+  }
   }
   
   sortBy(heading, order, i) {
+    //toggling icon
     if(order=='asc'){
       this.sortOrder[i]=false;
     }
     else{
       this.sortOrder[i]=true;
     }
+    //updating parameters
     this.parameters = {
       limit: 50, sortBy: heading, sortDir: order, startFrom: 0
     };
+    //resetting value
     this.tableData = [];
     this.count=1;
     this._router.navigate([],{queryParams:this.parameters});
+    //setting width
+    //calling data from backend
     this.serviceCall[this.functionCall](this.parameters).subscribe(res=>{
       this.tableData= this.tableData.concat(res);
     });
+    this.parameters = {
+      limit: 50,
+      sortBy: this.parameters.sortBy,
+      sortDir: this.parameters.sortDir,
+      startFrom: Number(this.parameters['limit']) + Number(this.parameters['startFrom'])
+    };
+    this._router.navigate([],{queryParams:this.parameters});
   }
+      private onMouseDown(event){
+      this.start = event.target;
+      this.pressed = true;
+      this.startX = event.x;
+      this.startWidth = $(this.start).parent().width();
+      this.initResizableColumns();
+      }
 
   private initResizableColumns() {
      this.renderer.listenGlobal('body', 'mousemove', (event) => {
@@ -105,6 +128,15 @@ export class TableGridComponent implements OnInit, OnChanges {
            let width = this.startWidth + (event.x - this.startX);
            $(this.start).parent().css({'min-width': width, 'max-width': width});
            let index = $(this.start).parent().index() + 1;
+           let nextIndex = $(this.start).parent().index() + 2;
+           if(width<this.columnWidth[index-1]){
+             this.columnWidth[nextIndex-1]=$('.divTableBody .divTableRow .divTableCell:nth-child(' + nextIndex + ')').width();
+             localStorage.setItem("columnWidth",this.columnWidth.join(","));
+             $('.divTableBody .divTableRow .divTableHead:nth-child(' + nextIndex + ')').css({'min-width': this.columnWidth[nextIndex-1]+this.columnWidth[index-1]-width, 'max-width': this.columnWidth[nextIndex-1]+this.columnWidth[index-1]-width});
+             $('.divTableBody .divTableRow .divTableCell:nth-child(' + nextIndex + ')').css({'min-width': this.columnWidth[nextIndex-1]+this.columnWidth[index-1]-width, 'max-width': this.columnWidth[nextIndex-1]+this.columnWidth[index-1]-width});
+           }
+           this.columnWidth[index-1]=width;
+           localStorage.setItem("columnWidth",this.columnWidth.join(","));
            $('.divTableBody .divTableRow .divTableCell:nth-child(' + index + ')').css({'min-width': width, 'max-width': width});
         }
      });
